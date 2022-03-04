038db823fc83cf3b93728854ee32ceaa0249d78b
Mthokozisi Dhlamini <63953399+sthabiso-iv@users.noreply.github.com>
2022-03-04 08:36:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class GameObject
+  {
+    private int id;
+    private String name;
+    public GameObject()
+    {
+      id = 0;
+      name = "";
+    }
+    public GameObject (int ID, String NAME)
+    {
+      id = ID;
+      name = NAME;
+    }
+  }
